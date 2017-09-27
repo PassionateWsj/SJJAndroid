acd55b10e95242ec27e79dc879842f9944de57e2
JamesWong <jameswong@yinzhendongdeMac-mini.local>
2017-09-27 12:53:57 +0800

    职业选择点击事件

--- app/src/main/java/com/intfocus/sjjandroid/login/ChooseCareerActivity.kt
+++ app/src/main/java/com/intfocus/sjjandroid/login/ChooseCareerActivity.kt
@@ -1,14 +1,31 @@
 package com.intfocus.sjjandroid.login
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
+import android.support.v7.app.AppCompatActivity
+import android.view.View
 import com.intfocus.sjjandroid.R
+import com.intfocus.sjjandroid.utils.ToastColor
+import com.intfoucs.sjjandroid.ToastUtils
 
 class ChooseCareerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose_career)
+    }
+
+    fun clickCareer(view: View) {
+        when (view.id) {
+            R.id.iv_choose_career_student -> {
+                ToastUtils.show(this, "学生",ToastColor.SUCCESS)
+            }
+            R.id.iv_choose_career_data_man -> {
+                ToastUtils.show(this, "数据人",ToastColor.SUCCESS)
+            }
+            R.id.iv_choose_career_running -> {
+                ToastUtils.show(this, "运营汪",ToastColor.SUCCESS)
+            }
+
+        }
     }
 }
